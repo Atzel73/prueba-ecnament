667c1d1ed8f16a55b1e2c5a6882f9968b55781eb
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-10-04 20:32:16 -0600

    funcionando: faltan correcciones de logica

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { isEven } from 'src/assets/functions/NumberEven';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  //Se declara inputNumber de tipo numerico, la cual de declara de tipo numerico
+  //Y su valor se asigna en tabs.page.html
+  inputNumber: number | undefined;
+  isChecked: boolean | undefined;
+  //Se crea la variable result de tipo objeto, y cada variable dentro de el es de tipo objeto
+  //Guardando en cada una el resultado de la funcion
+  result:
+    | { even3: number[]; even5: number[]; even7: number[]; notEven: number[] }
+    | undefined;
 
   constructor() {}
 
+  //Se declara getNumbers() de manera local
+  getNumbers() {
+    //Si inputNumber tiene un valor, se llamara a isEven()
+    //Y se enviara esta misma como parametro
+    if (this.inputNumber) {
+      const result = isEven(this.inputNumber);
+
+      //Para despues, en cada propiedad de result, asignar el valor que
+      //Esta regresando la funcion isEven()
+      this.result = {
+        even3: result.even3,
+        even5: result.even5,
+        even7: result.even7,
+        notEven: result.notEven,
+      };
+      this.isChecked = false;
+      //Despues de mostrar el resultado
+      //Se llama la funcion para subirlo a la base de datos
+
+
+      //Despues de guardar los datos, se activa un Toast de confirmacion
+    } else if (!this.inputNumber) {
+      this.isChecked = true;
+    }
+  }
 }
---
+++ src/assets/functions/NumberEven.ts
@@ -0,0 +1,26 @@
+export function isEven(n: number) {
+  //Se declaran las variables de tipo arreglo para almacenar
+  //Los numeros que cumplan con la condicion
+  let even3 = [];
+  let even5 = [];
+  let even7 = [];
+  let notEven = [];
+
+  //Se usa un ciclo for para dar las vueltas en base al numero
+  //Entrante por parametro
+  for (let index = 1; index <= n; index++) {
+    if (index % 3 === 0) {
+      even3.push(index);
+    }
+    if (index % 5 === 0) {
+      even5.push(index);
+    }
+    if (index % 7 === 0) {
+      even7.push(index);
+    }
+    if (index % 3 !== 0 || index % 5 === 0 || index % 7 === 0) {
+      notEven.push(index);
+    }
+  }
+  return { even3, even5, even7, notEven };
+}
