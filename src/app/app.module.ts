2edbc72a559c2c59eb0113ae99d5fd334bdeefae
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-10-05 19:31:49 -0600

    paquetes de firebase instalador: deploy no terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,7 +11,13 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA0V9dVsf7V1X7y6tCuKcjtN07fMHTKRkk",
+    authDomain: "prueba-ecnament.firebaseapp.com",
+    projectId: "prueba-ecnament",
+    storageBucket: "prueba-ecnament.appspot.com",
+    messagingSenderId: "285491921537",
+    appId: "1:285491921537:web:c9dd000cab04a22e88c380"
+  }
 };
